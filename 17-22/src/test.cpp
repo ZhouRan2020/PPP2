7d262332b461f37af395359bcdcb9bf226e02aae
ZhouRan2020 <rzhoujames@163.com>
2023-04-15 18:12:51 +0800

    CMake and Vscode and clang is important

--- 17-22/src/test.cpp
+++ 17-22/src/test.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+//#include <stop_token>
+//#include <stop_token>
 
 int main(){
     std::cout<<"hello.\n";
+    int y=000;
+   // in rrrr=9kfs;
+ //   std::stop_token st{};
 }
